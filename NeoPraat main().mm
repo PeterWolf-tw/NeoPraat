e72d05c87c4fa0003fa066d27aa8a373ea5635ba
PeterWolf <peterwolf.wang@gmail.com>
2014-12-06 02:26:30 +0800

    Delete NeoPraat main().mm

--- NeoPraat main().mm
+++
@@ -1,4 +0,0 @@
-<map version="0.9.0">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1417184136014" ID="ID_636168519" MODIFIED="1417184147111" TEXT="NeoPraat: main()"/>
-</map>
