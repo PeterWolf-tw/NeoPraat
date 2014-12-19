0dcaa63b2eea6605f8df4f6a6049dc61b204862d
Peter.Wolf <peterwolf.wang@gmail.com>
2014-12-19 21:57:14 +0800

    新增 MenuBar

--- NeoPraat.py
+++ NeoPraat.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
+import json
+import os
 import sys
+from PySide import QtCore
+from PySide import QtGui
 from PySide.QtCore import *
 from PySide.QtGui import *
 # Every Qt application must have one and only one QApplication object;
 # it receives the command line arguments passed to the script, as they
 # can be used to customize the application's appearance and behavior
 qt_app = QApplication(sys.argv)
- 
+
+class multiLanguage:
+    def __init__(self):
+        if os.path.exists("./locale"):
+            pass
+        else:
+            print("Error, Cannot find language settings")
+    def languageSelector(self):
+        '''Create a selection window based on the LANGUAGE.json file found under locale directory.'''
+        localeLIST = [x for x in os.listdir("./locale/") if x.endswith("json")]
+    def languageSetting(self):
+        return json.loads(open("./NeoPraat.cfg").read())["locale"]
+        
+
 class MainWindow(QWidget):
     ''' An example of PySide absolute positioning; the main window
         inherits from QWidget, a convenient widget for an empty window. '''
@@ -19,73 +37,43 @@ class MainWindow(QWidget):
         # ourselves, since we control the entire layout
         self.setMinimumSize(600, 700)
         self.setWindowTitle('NeoPraat')
- 
+        self.createMenu()
         # Create the controls with this object as their parent and set
         # their position individually; each row is a label followed by
         # another control
- 
+        mainLayout = QtGui.QVBoxLayout()
+        mainLayout.setMenuBar(self.menuBar) 
+        self.setLayout(mainLayout)
         # Label for the salutation chooser
-        self.salutation_lbl = QLabel('Salutation:', self)
-        self.salutation_lbl.move(5, 5) # offset the first control 5px
-                                       # from top and left
-        self.salutations = ['Ahoy',
-                            'Good day',
-                            'Hello',
-                            'Heyo',
-                            'Hi',
-                            'Salutations',
-                            'Wassup',
-                            'Yo']
-        # Create and fill the combo box to choose the salutation
-        self.salutation = QComboBox(self)
-        self.salutation.addItems(self.salutations)
- 
-        # Allow 100px for the label and 5px each for borders at the
-        # far left, between the label and the combobox, and at the far
-        # right
-        self.salutation.setMinimumWidth(285)
-        # Place it five pixels to the right of the end of the label
-        self.salutation.move(110, 5)
- 
-        # The label for the recipient control
-        self.recipient_lbl = QLabel('Recipient:', self)
-        # 5 pixel indent, 25 pixels lower than last pair of widgets
-        self.recipient_lbl.move(5, 30)
- 
-        # The recipient control is an entry textbox
-        self.recipient = QLineEdit(self)
-        # Add some ghost text to indicate what sort of thing to enter
-        self.recipient.setPlaceholderText('world')
-        # Same width as the salutation
-        self.recipient.setMinimumWidth(285)
-        # Same indent as salutation but 25 pixels lower
-        self.recipient.move(110, 30)
- 
-        # The label for the greeting widget
-        self.greeting_lbl = QLabel('Greeting:', self)
-        # Same indent as the others, but 45 pixels lower so it has
-        # physical separation, indicating difference of function
-        self.greeting_lbl.move(5, 75)
- 
-        # The greeting widget is also a label
-        self.greeting = QLabel('', self)
-        # Same indent as the other controls
-        self.greeting.move(110, 75)
- 
-        # The build button is a push button
-        self.build_button = QPushButton('Build Greeting', self)
- 
-        # Place it at the bottom right, narrower than
-        # the other interactive widgets
-        self.build_button.setMinimumWidth(145)
-        self.build_button.move(250, 150)
- 
+        
     def run(self):
         # Show the form
         self.show()
         # Run the Qt application
         qt_app.exec_()
- 
-# Create an instance of the application window and run it
-app = MainWindow()
-app.run()
+        
+    def createMenu(self):
+        multiLang = multiLanguage()
+        locale = json.loads(open("./locale/{0}.json".format(multiLang.languageSetting())).read())
+        self.menuBar       = QtGui.QMenuBar()
+        self.neoPraatMenu  = QtGui.QMenu(locale["NeoPraat"], self)
+        self.aboutNeoPraat = self.neoPraatMenu.addAction(locale["About NeoPraat"])
+        self.newScript     = self.neoPraatMenu.addAction(locale["New Script"])
+        self.openScript    = self.neoPraatMenu.addAction(locale["Open Script"])
+        self.goodies       = self.neoPraatMenu.addMenu(locale["Goodies"])
+        self.preferences   = self.neoPraatMenu.addAction(locale["Preferences"])
+        self.technical     = self.neoPraatMenu.addMenu(locale["Technical"])
+        self.exit          = self.neoPraatMenu.addAction(locale["Quit"])
+        self.openMenu = QtGui.QMenu(locale["Open"])
+        self.saveMenu = QtGui.QMenu(locale["Save"])
+        self.helpMenu = QtGui.QMenu(locale["Help"])
+        self.menuBar.addMenu(self.neoPraatMenu)
+        self.menuBar.addMenu(self.openMenu)
+        self.menuBar.addMenu(self.saveMenu)
+        self.menuBar.addMenu(self.helpMenu)
+
+        
+if __name__ == '__main__':
+    # Create an instance of the application window and run it
+    app = MainWindow()
+    app.run()
